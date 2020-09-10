e99d053f782750e55e540dc3d65bae3374898dd5
chriscauley <chris@lablackey.com>
2020-09-10 09:29:50 +0000

    dev server port

--- client/manage.py
+++ client/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management.commands.runserver import Command as runserver
 
 def main():
     """Run administrative tasks."""
@@ -15,8 +15,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    runserver.default_port = "8548"
+    runserver.default_addr = "0.0.0.0"
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
